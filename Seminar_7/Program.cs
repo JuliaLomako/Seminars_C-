a272b8ac508cfac3f98a85adf12422c806d41fe5
Julia <jlomako@yandex.ru>
2022-04-21 09:48:26 +0300

    задачи

--- Seminar_7/Program.cs
+++ Seminar_7/Program.cs
@@ -105,3 +105,18 @@ SearchElement();
 // Например, задан массив:
 
 
+
+int[,] FillMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
