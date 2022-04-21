b093bd70014ea7e423e10cc9f6a5c31f3053c5ea
Julia <jlomako@yandex.ru>
2022-04-21 16:05:33 +0300

    задачи_семинар

--- Seminar_7/Program.cs
+++ Seminar_7/Program.cs
@@ -1,49 +1,89 @@
-﻿// Задача 1: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми двузначныеми числами.
 
-// void FillArrayRandom (int m, int n);
-// {
-//     int [,] matrix = new int [m, n];
-//     for (int i = 0; i < m; i++)
-//     {
-//         for (int j = 0; j < n; j++)
-//         {
-//             matrix[i,j] = new Random().Next(-99, 100);
-//             Console.Write($"{matrix[i,j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-
-// FillArrayRandom(2,3);
-
-// Задача 2: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: 
-// Aₘₙ = m+n. Выведите полученный массив на экран.
+void NewRandomArray (int m, int n)
+{
+    int [,] matrix = new int [m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix [i, j] = new Random().Next(-99, 100);
+            Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+NewRandomArray (2, 6);
 
 
-// void FillArraySumm(int m, int n);
-// {
-//     int[,] matrix = new int[m, n];
-//     for (int i = 0; i < m; i++)
-//     {
-//         for (int j = 0; j < n; j++)
-//         {
-//             matrix[i, j] = i + j;
-//             Console.Write($"{matrix[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
+// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по 
+// формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
 
-// FillArraySumm(2, 3);
+void FindElementInArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = i + j;
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+FindElementInArray(2, 6);
 
 
-// Задача 3:Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
-// и замените эти элементы на их квадраты.
+// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените 
+// эти элементы на их квадраты.
 
-// void FillArray(int m, double n)
-// {
-//     double[,] matrixArray = new double[m, n];
+void FindBothPositiveElements (int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix [i, j] = new Random().Next(1, 10);
+            if (i%2 == 0 && j%2 == 0)
+            {
+                matrix [i, j] =  matrix[i, j] * matrix[i, j];
+            }
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+FindBothPositiveElements (3, 3);
 
+
+// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
+
+void SumOfElementsOnMainDiagonal (int m)
+{
+    int sum = 0;
+    int[,] matrix = new int[m, m];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+            if (i == j)
+            {
+
+                sum = sum + matrix[i, j];
+            }
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("The sum is: " + sum);
+}
+
+SumOfElementsOnMainDiagonal (3);
 
 
 
